d1b0b51e0a32d39e6f3ef096c68a08f8ad83c2b8
ArthurLeroy <arthur.leroy.pro@gmail.com>
2021-10-15 01:41:37 +0200

    Add graph 5

--- Graphes-these-5.R
+++ Graphes-these-5.R
@@ -3,67 +3,71 @@ load("Arabido_imp_data_lg")
 
 source("bayes_proteo.R")
 
-# pept_ups = "AALEELVK" 
-# pept_arath = "EVQELAQEAAER" 
+pept_ups = "AALEELVK"
+pept_arath = "EVQELAQEAAER"
 # db_pept_ups = db.raw %>% filter(ID == pept_ups) %>% drop_na()
 # db_pept_arath = db.raw %>% filter(ID == pept_arath) %>% drop_na()
-# 
-# db_pept_ups_imp = data.lg %>% 
-#   dplyr::rename(Draw = Imp.Draw, Output = Intensity) %>% 
-#   filter(ID == pept_ups) %>% 
-#   group_by(Sample) %>%
-#   summarise(ID = max(ID), Group = max(Group), Output = mean(Output))
-# 
-# db_pept_arath_imp = data.lg %>% 
-#   dplyr::rename(Draw = Imp.Draw, Output = Intensity) %>% 
-#   filter(ID == pept_arath) %>% 
-#   group_by(Sample) %>%
-#   summarise(ID = max(ID), Group = max(Group), Output = mean(Output))
-# 
-# ## Graph comparison with and without correlations
-# res_graph3_ups_uni = post_mean_diff_uni(
-#   data = db_pept_ups,
-#   mu_0 = 20, 
-#   lambda_0 = 1,
-#   beta_0 = 1,
-#   alpha_0 = 2
-# )
-# res_graph3_ups_uni_imp = post_mean_diff_uni(
-#   data = db_pept_ups_imp,
-#   mu_0 = 20, 
-#   lambda_0 = 1,
-#   beta_0 = 1,
-#   alpha_0 = 1
-# )
-# 
-# res_graph3_arath_uni = post_mean_diff_uni(
-#   data = db_pept_arath,
-#   mu_0 = 20, 
-#   lambda_0 = 1,
-#   beta_0 = 1,
-#   alpha_0 = 2
-# )
-# res_graph3_arath_uni_imp = post_mean_diff_uni(
-#   data = db_pept_arath_imp,
-#   mu_0 = 20, 
-#   lambda_0 = 1,
-#   beta_0 = 1,
-#   alpha_0 = 1
-# )
-# 
-# gg5_1 = plot_dif(res_graph3_ups_uni, c('Point1', 'Point7'), peptide = pept_ups) +
-#   xlim(c(-1,10)) + ylim(c(0,0.4))
-# xlab(TeX('$\\mu_1 - \\mu_7$ - ups'))
-# gg5_2 = plot_dif(res_graph3_ups_uni_imp, c('Point1', 'Point7'), peptide = pept_ups) +
-#   xlim(c(-1,10)) + ylim(c(0,0.4)) 
-# xlab(TeX('$\\mu_1 - \\mu_7$- ups - imp'))
-# gg5_3 = plot_dif(res_graph3_arath_uni, c('Point1', 'Point7'), peptide = pept_arath) +
-#   xlim(c(-2,3)) + ylim(c(0,0.9))
-# xlab(TeX('$\\mu_1 - \\mu_7$- arath'))
-# gg5_4 = plot_dif(res_graph3_arath_uni_imp, c('Point1', 'Point7'), peptide = pept_arath) +
-#   xlim(c(-2,3)) + ylim(c(0,0.9)) 
-# xlab(TeX('$\\mu_1 - \\mu_7$'))
-# 
-# #png('FIGURES/ch5_graph5.png', res = 600, width = 4800, height = 3600, units = "px")
-# cowplot::plot_grid(gg3_1, gg3_3, gg3_2, gg3_4, nrow = 2, ncol = 2)
-# #dev.off()
+
+db_prot_ups = data.lg %>% 
+  dplyr::rename(Draw = Imp.Draw, Output = Intensity) %>% 
+  filter(Protein == "P12081ups|SYHC_HUMAN_UPS") %>% drop_na()
+db_prot_arath = data.lg  %>% 
+  dplyr::rename(Draw = Imp.Draw, Output = Intensity) %>%
+  filter(Protein == "sp|F4I893|ILA_ARATH") %>% drop_na()
+
+## Graph comparison with and without correlations
+dim_prot_ups = db_prot_ups$ID %>% n_distinct()
+mean_ups = db_prot_ups$Output %>% mean()
+mean_arath = db_prot_arath$Output %>% mean()
+
+res_graph5_ups_multi = post_mean_diff(
+  data = db_prot_ups,
+  mu_0 = rep(mean_ups, dim_prot_ups), 
+  lambda_0 = 1,
+  Sigma_0 = diag(1, ncol = dim_prot_ups, nrow = dim_prot_ups),
+  nu_0 = 10
+)
+
+dim_prot_arath = db_prot_arath$ID %>% n_distinct()
+
+res_graph5_arath_multi = post_mean_diff(
+  data = db_prot_arath,
+  mu_0 = rep(mean_arath, dim_prot_arath), 
+  lambda_0 = 1,
+  Sigma_0 = diag(1, ncol = dim_prot_arath, nrow = dim_prot_arath),
+  nu_0 = 10
+)
+
+list_pept = db_prot_ups$ID %>% unique()
+gg5_1 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[1]) +
+  xlim(c(-7,6)) + ylim(c(0,1.8)) +
+  xlab(list_pept[1])
+gg5_2 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[2]) +
+  xlim(c(-7,6)) + ylim(c(0,1.8)) +
+  xlab(list_pept[2])
+gg5_3 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[3]) +
+  xlim(c(-7,6)) + ylim(c(0,1.8)) +
+  xlab(list_pept[3])
+gg5_4 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[4]) +
+  xlim(c(-7,6)) + ylim(c(0,1.8)) +
+  xlab(list_pept[4])
+gg5_5 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[5]) +
+  xlim(c(-7,6)) + ylim(c(0,1.8)) +
+  xlab(list_pept[5])
+gg5_6 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[6]) +
+  xlim(c(-7,6)) + ylim(c(0,1.8)) +
+  xlab(list_pept[6])
+gg5_7 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[7]) +
+  xlim(c(-7,6)) + ylim(c(0,1.8)) +
+  xlab(list_pept[7])
+gg5_8 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[8]) +
+  xlim(c(-7,6)) + ylim(c(0,1.8)) +
+  xlab(list_pept[8])
+gg5_9 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[9]) +
+  xlim(c(-7,6)) + ylim(c(0,1.8)) +
+  xlab(list_pept[9])
+
+png('FIGURES/ch5_graph5.png', res = 600, width = 6000, height = 6000, units = "px")
+cowplot::plot_grid(gg5_1, gg5_3, gg5_5, gg5_7, gg5_9,
+                   gg5_2, gg5_4, gg5_6, gg5_8, nrow = 3, ncol = 3)
+dev.off()
