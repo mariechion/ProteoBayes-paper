4718894b39e6c62f7a0119d1e6332eb02129fef4
ArthurLeroy <arthur.leroy.pro@gmail.com>
2023-07-17 12:45:25 +0200

    Add gif ProteoBayes

--- Simulation_paper.R
+++ Simulation_paper.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(ProteoBayes)
+library(gganimate)
 
 #### Useful functions#####
 
@@ -15,11 +16,11 @@ simu_data = function(
     'Group' = rep(rep(1:nb_group, each = nb_sample),nb_peptide),
     'Sample' = rep(rep( 1:nb_sample, nb_group*nb_peptide))
     ) %>%
-    dplyr::group_by(.data$Peptide) %>%
+    dplyr::group_by(Peptide) %>%
     dplyr::mutate(
       'Mean' = runif(1, range_peptide[1], range_peptide[2]) +
-        list_mean_diff[.data$Group],
-      'Output' = .data$Mean + rnorm(n(), 0, list_var[.data$Group])
+        list_mean_diff[Group],
+      'Output' = Mean + rnorm(n(), 0, list_var[Group])
       ) %>%
     ungroup() %>%
   return()
@@ -38,19 +39,9 @@ eval <- function(
     list_mean_diff = list_mean_diff,
     list_var = list_var)
   
-  t_test =  db %>%
-    tidyr::expand_grid('Group2' = unique(.data$Group)) %>%
-    dplyr::left_join(
-      db %>%
-        dplyr::select(- Mean) %>%
-        dplyr::rename('Group2' = .data$Group,
-                      'Output2' = .data$Output),
-        by = c("Peptide", 'Group2', 'Sample')) %>%
-    dplyr::filter(Group == 1, Group2 != 1) %>%
-    dplyr::group_by(Peptide, Group, Group2) %>%
-    dplyr::reframe('p_value' = t.test(Output, Output2)$p.value,
-                    'Signif' = (p_value < 0.05))
-  db %>%
+  t_test = multi_t_test(db)
+  
+  res = db %>%
     posterior_mean() %>%
     identify_diff() %>%
     dplyr::filter(Group == 1) %>%
@@ -61,14 +52,15 @@ eval <- function(
     mutate('MSE' = (Mean - mu2)^2,
                          'CIC' = ((Mean > CI_inf2) & (Mean < CI_sup2)) * 100,
                          'Diff_mean' = abs(mu2 - mu)) %>%
-    left_join(t_test, by = c('Peptide', 'Group', 'Group2')) %>%
+    left_join(t_test, by = c('Peptide', 'Group', 'Group2')) %>% 
     return()
 }
 
 summarise_eval <- function(eval){
   eval %>%
     group_by(Group, Group2) %>%
-    summarise(across(c(MSE, CIC, Diff_mean, Distinct, p_value, Signif),
+    summarise(across(c(MSE, CIC, Diff_mean, Distinct,
+                       p_value, Signif),
                      .fns = list('Mean' = mean, 'Sd' = sd))) %>%
     mutate('MSE_Mean' = sqrt(MSE_Mean), 'MSE_Sd' = sqrt(MSE_Sd)) %>%
     mutate(across(MSE_Mean:Signif_Sd, ~ round(.x, 2))) %>%
@@ -77,8 +69,23 @@ summarise_eval <- function(eval){
             'CIC' =  paste0(CIC_Mean, ' (', CIC_Sd, ')'),
             'Diff_mean' =  paste0(Diff_mean_Mean, ' (', Diff_mean_Sd, ')'),
             'Distinct' =  paste0(Distinct_Mean*100, ' (', Distinct_Sd*100, ')'),
-            'p_value' =  paste0(p_value_Mean, ' (', p_value_Sd, ')'),
-            'Signif' =  paste0(Signif_Mean*100, ' (', Signif_Sd*100, ')')) %>%
+            'p_value' =  paste0(p_value_Mean, ' (', p_value_Sd, ')')) %>%
+    return()
+}
+
+multi_t_test <- function(data){
+  data %>%
+    tidyr::expand_grid('Group2' = unique(Group)) %>%
+    dplyr::left_join(
+      data %>%
+        dplyr::select(- Mean) %>%
+        dplyr::rename('Group2' = Group,
+                      'Output2' = Output),
+      by = c("Peptide", 'Group2', 'Sample')) %>%
+    dplyr::filter(Group == 1, Group2 != 1) %>%
+    dplyr::group_by(Peptide, Group, Group2) %>%
+    dplyr::reframe('p_value' = t.test(Output, Output2)$p.value,
+                   'Signif' = (p_value < 0.05)) %>% 
     return()
 }
 
@@ -94,17 +101,106 @@ res1 = eval(
   list_var = c(1, 1, 1, 1, 1)
   )
 
-summarise_eval(res1)
+co = summarise_eval(res1)
 
 ## Experiment 2: Evaluation of posteriors for different variances 
-
-set.seed(42)
+set.seed(1)
 
 res2 = eval(
   nb_peptide = 1000,
-  nb_sample = 100,
+  nb_sample = 5,
   list_mean_diff = c(0, 1, 1, 1, 1),
   list_var = c(1, 1, 5, 10, 20)
-  )
+)
 
 summarise_eval(res2)
+
+## Experiment 3: Evaluation of posteriors for different variances 
+
+## Experiment 4: Evaluation of running times
+set.seed(1)
+res4 = c()
+for(i in c(10, 100, 1000)){
+  
+  data = simu_data(nb_peptide = i)
+  
+  t1 = Sys.time()
+  dummy = posterior_mean(data)
+  t2 = Sys.time()
+  dummy = multi_t_test(data) 
+  t3 = Sys.time()
+  #dummy = multi_posterior_mean(data)
+  t4 = Sys.time()
+  
+   res4 = bind_rows(
+     res4, 
+     tibble('Nb_peptide' = i, 
+            'Time_t_test' = (t2 - t1) %>% round(2),
+            'Time' = (t3 - t2) %>% round(2),
+            'Time_multi' = (t4 - t3) %>% round(2))
+     ) 
+}
+
+## Experiment 5: Evaluation of the uncertainty bias coming from imputation 
+
+#### GIF ProteoBayes visualisation ####
+set.seed(1)
+nb_sample = 5
+data = simu_data(nb_peptide = 1, 
+                 nb_sample = nb_sample,
+                 list_mean_diff = c(-2, 5),
+                 list_var = c(1, 3),
+                 range_peptide = c(10,10))
+
+## Prepare the data and posterior distributions for animation
+data_anim <- tibble::tibble()
+post_anim <- tibble::tibble()
+for (j in 1:n_distinct(data$Sample)) {
+  
+  data_anim = data %>%
+    dplyr::filter(Sample %in% 1:j) %>% 
+    dplyr::mutate('Index' = j) %>% 
+    mutate('Group' = as.factor(Group)) %>%
+    dplyr::bind_rows(data_anim)
+  
+  ## Extract the sample of the 'j' first data points
+  post_anim <- data %>%
+    dplyr::filter(Sample %in% 1:j) %>% 
+    posterior_mean(mu_0 = 10, lambda_0 = 1, alpha_0 = 1, beta_0 = 1) %>% 
+    dplyr::mutate('Index' = j) %>%
+    dplyr::bind_rows(post_anim)
+}
+
+## Define the prior distribution
+prior = tibble('Peptide' = 'Peptide_1', 'Group' = 1:2 , 'mu' = 10, 
+               'lambda' = 1, 'alpha' = 1, 'beta' = 1, 'Index' = 0)
+
+## Sample empirical distributions from the posteriors
+samples = post_anim %>%
+  bind_rows(prior) %>% 
+  group_by(Group, Index) %>% 
+  sample_distrib(nb_sample = 100000) %>%
+  mutate('Group' = as.factor(Group))
+
+## Sample empirical distributions from the priors
+samples_prior = prior %>%
+  uncount(6) %>%
+  mutate(Index = rep(0:5, 2)) %>% 
+  group_by(Group, Index) %>% 
+  sample_distrib(nb_sample = 100000)
+
+## Create the animation
+gg = ggplot(samples) +
+  geom_density(data=samples_prior, aes(x = Sample), fill = 'black', alpha=0.3) +
+  geom_density(aes(x = Sample, fill = Group), alpha = 0.6)  + 
+  geom_point(data = data_anim, aes(x = Output, y = 0, col = Group), size = 3) + 
+  theme_classic() + xlim(2, 21) + xlab('Output') + ylab('Density') + 
+  gganimate::transition_states(Index)
+
+
+gg_anim = animate(gg, height = 1800, width = 3200, res = 300)
+
+
+anim_save('anim_ProteoBayes_wide2.gif', gg_anim) 
+  
+
