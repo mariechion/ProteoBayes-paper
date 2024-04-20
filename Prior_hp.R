99bb76db0fa53df96b42a4621033e06a8eded2bd
ArthurLeroy <arthur.leroy.pro@gmail.com>
2024-04-20 21:08:03 +0200

    Add functions to visualise effect of priors

--- Prior_hp.R
+++ Prior_hp.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(ProteoBayes)
 library(extraDistr)
+library(gganimate)
 
 # Sampling from a Gaussian-inverse-Gamma distribution
 rnorm_inv_gamma <- function(n, mu_0, lambda_0, alpha_0, beta_0){
@@ -35,4 +36,55 @@ obs_hp <- db_hp %>%
 
 ggplot(obs_hp, aes(y = mu, x = lambda_0, col = experiment)) +
   geom_violin()
+
+#### Arthur
+
+nb_sample = 1000
+lambda = 1
+alpha = 1
+beta = 1
+
+sample_prior = function(nb_sample, alpha, beta, lambda, param, seq_param)
+{
+  floop = function(i){
+    alpha = if_else(param == 'alpha', i, alpha)
+    beta = if_else(param == 'beta', i, beta)
+    lambda = if_else(param == 'lambda', i, lambda)
+
+    sigma = rinvgamma(n = nb_sample, alpha = alpha, beta = beta)
+    mu = rnorm(nb_sample, mean = rep(0, nb_sample), sd = sqrt((1/lambda) * sigma))
+    
+    param_value = case_when(
+      param == 'alpha' ~ alpha, 
+      param == 'beta' ~ beta,
+      param == 'lambda' ~ lambda
+    )
+    
+    tibble(Mean = mu, Param = i) %>% 
+      return()
+  }
+  lapply(seq_param, floop) %>% 
+    bind_rows() %>%
+    return()
+}
+
+
+text_param = 'beta'
+db = sample_prior(nb_sample, alpha, beta, lambda, text_param, seq(0.1, 10, 1))
+
+
+gif = ggplot(db) + 
+  geom_density(aes(x = Mean), col = "#DB15C1", fill = "#FA9FB5") + 
+  geom_text(aes(x= -10, y= 0.3,
+                label=paste("Value of",text_param , ": ", Param)), 
+            hjust=0)+
+  geom_vline(aes(xintercept = 0, col = "black")) +
+  theme_classic() + xlim(c(-10, 10)) + 
+  transition_states(Param,
+                    transition_length = 2,
+                    state_length = 1)
+
+
+
+ 
 
