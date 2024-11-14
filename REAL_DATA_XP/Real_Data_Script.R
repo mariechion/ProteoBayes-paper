45bad318b1fe3f28d22386dc58d36cefa088fe0a
Marie Chion <marie.chion@protonmail.com>
2024-11-14 17:13:46 +0000

    Update Real_Data_Script.R

--- REAL_DATA_XP/Real_Data_Script.R
+++ REAL_DATA_XP/Real_Data_Script.R
@@ -11,37 +11,30 @@ source("REAL_DATA_XP/Functions.R")
 # -- EXPERIMENTS -- #
 set.seed(17)
 
+prop_NA = 0.2
+multi = F
+mu_0 = NULL
+lambda_0 = 1e-10
+beta_0 = 0.01
+alpha_0 = 0.01
+alpha = 0.05
+FDR = NULL
+
 db_ARATH <- read.delim("REAL_DATA_XP/Arabido_UPS/peptides.txt")
-res_ARATH <- real_data_eval(data = db_ARATH, type = "ARATH",
-                            prop_NA = 0.2,
-                            multi = F,
-                            mu_0 = NULL, 
-                            lambda_0 = 1e-10, beta_0 = 0.01, alpha_0 = 0.01,
-                            alpha = 0.05, FDR = NULL)
+res_ARATH <- real_data_eval(data = db_ARATH, type = "ARATH", maxquant = T)
 
 ARATH_DiffMean <- res_ARATH$results$DiffMean
 ARATH_EstimQual <- res_ARATH$results$EstimQual
 
 db_YST <- read.delim("REAL_DATA_XP/Yeast_UPS/peptides.txt")
-res_YST <- real_data_eval(data = db_YST, type = "YST",
-                          prop_NA = 0.2,
-                          multi = F,
-                          mu_0 = NULL, 
-                          lambda_0 = 1e-10, beta_0 = 0.01, alpha_0 = 0.01,
-                          alpha = 0.05, FDR = NULL)
+res_YST <- real_data_eval(data = db_YST, type = "YST", maxquant = T)
 
 YST_DiffMean <- res_YST$results$DiffMean
 YST_EstimQual <- res_YST$results$EstimQual
 
 db_MOUSE <- read_delim("REAL_DATA_XP/Mouse_UPS/Spike-in-biol-var-OT-SN-Report.txt", 
                        delim = "\t", escape_double = FALSE, trim_ws = TRUE)
-res_MOUSE <- real_data_eval(data = db_MOUSE, type = "MOUSE",
-                            maxquant = F,
-                            prop_NA = 0.2,
-                            multi = F,
-                            mu_0 = NULL, 
-                            lambda_0 = 1e-10, beta_0 = 0.01, alpha_0 = 0.01,
-                            alpha = 0.05, FDR = NULL)
+res_MOUSE <- real_data_eval(data = db_MOUSE, type = "MOUSE", maxquant = F)
 
 
 MOUSE_DiffMean <- res_MOUSE$results$DiffMean
@@ -49,12 +42,7 @@ MOUSE_EstimQual <- res_MOUSE$results$EstimQual
 
 
 db_YST_B <- read.delim("~/Documents/GitHub/ProteoBayes_draft/REAL_DATA_XP/Yeast_UPS_B/peptides.txt")
-res_YST_B <- real_data_eval(data = db_YST_B, type = "YST_B",
-                          prop_NA = 0.2,
-                          multi = F,
-                          mu_0 = NULL, 
-                          lambda_0 = 1e-10, beta_0 = 0.01, alpha_0 = 0.01,
-                          alpha = 0.05, FDR = NULL)
+res_YST_B <- real_data_eval(data = db_YST_B, type = "YST_B", maxquant = T)
 
 
 YST_B_DiffMean <- res_YST_B$results$DiffMean
