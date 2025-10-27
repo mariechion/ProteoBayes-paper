a55c229b8834c8a69f1ef77e9636b34f448d24c0
Arthur <arthur.leroy@inrae.fr>
2025-10-27 14:35:28 +0100

    Create new marginals multivariate graph

--- Graphes-these-5.R
+++ Graphes-these-5.R
@@ -26,76 +26,60 @@ dim_prot_ups = db_prot_ups$ID %>% n_distinct()
 mean_ups = db_prot_ups$Output %>% mean()
 mean_arath = db_prot_arath$Output %>% mean()
 
-res_graph5_ups_multi = post_mean_diff(
-  data = db_prot_ups,
-  mu_0 = rep(mean_ups, dim_prot_ups), 
-  lambda_0 = 1,
-  Sigma_0 = diag(1, ncol = dim_prot_ups, nrow = dim_prot_ups),
-  nu_0 = 10
-)
-
-dim_prot_arath = db_prot_arath$ID %>% n_distinct()
-
-res_graph5_arath_multi = post_mean_diff(
-  data = db_prot_arath,
-  mu_0 = rep(mean_arath, dim_prot_arath), 
-  lambda_0 = 1,
-  Sigma_0 = diag(1, ncol = dim_prot_arath, nrow = dim_prot_arath),
-  nu_0 = 10
-)
-
-list_pept = db_prot_ups$ID %>% unique()
-gg5_1 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[1]) +
-  xlim(c(-7,6)) + ylim(c(0,1.8)) +
-  xlab(list_pept[1])
-gg5_2 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[2]) +
-  xlim(c(-7,6)) + ylim(c(0,1.8)) +
-  xlab(list_pept[2])
-gg5_3 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[3]) +
-  xlim(c(-7,6)) + ylim(c(0,1.8)) +
-  xlab(list_pept[3])
-gg5_4 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[4]) +
-  xlim(c(-7,6)) + ylim(c(0,1.8)) +
-  xlab(list_pept[4])
-gg5_5 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[5]) +
-  xlim(c(-7,6)) + ylim(c(0,1.8)) +
-  xlab(list_pept[5])
-gg5_6 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[6]) +
-  xlim(c(-7,6)) + ylim(c(0,1.8)) +
-  xlab(list_pept[6])
-gg5_7 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[7]) +
-  xlim(c(-7,6)) + ylim(c(0,1.8)) +
-  xlab(list_pept[7])
-gg5_8 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[8]) +
-  xlim(c(-7,6)) + ylim(c(0,1.8)) +
-  xlab(list_pept[8])
-gg5_9 = plot_dif(res_graph5_ups_multi, c('Point1', 'Point7'), peptide = list_pept[9]) +
-  xlim(c(-7,6)) + ylim(c(0,1.8)) +
-  xlab(list_pept[9])
-
-png('FIGURES/ch5_graph5.png', res = 600, width = 6400, height = 3600, units = "px")
-cowplot::plot_grid(gg5_1, gg5_3, gg5_5, gg5_7, gg5_9,
-                   gg5_2, gg5_4, gg5_6, gg5_8, nrow = 3, ncol = 3)
-dev.off()
-
 #### New version of multivariate ProteoBayes ####
 
-db = db_prot_ups %>%
+db = db_prot_arath %>%
   select(-Protein) %>%
   rename(Peptide = ID) %>% 
   mutate(Peptide = substr(Peptide, 1, 4)) %>% 
   mutate(Group = substr(Group, 6, 7)) %>% 
-  filter(Group %in% c("1", "4", "7"))
+  filter(Group %in% c("1", "7"))
 
 post_arath = multi_posterior_mean(
   data = db, 
   mu_0 = NULL,
-  lambda_0 = 1,
+  lambda_0 = 1e-10,
   nu_0 = 10)
 
 diff = multi_identify_diff(post_arath)
 
+## Graph of multivariate differences
 gg = plot_multi_diff(diff)
 
-ggsave("FIGURES/multi_diff_diff.png", gg,
-       dpi = 600, width = 6, height = 6)
+# ggsave("FIGURES/multi_diff_diff.png", gg,
+#        dpi = 600, width = 6, height = 6)
+
+## Graph of marginal differences for each peptide
+sample = sample_distrib(post_arath, n = 10000)
+list_pept = db$Peptide %>% unique()
+p1 = plot_distrib(sample, group1 = "1", group2 = "7", peptide = list_pept[1]) +
+  xlim(c(-2,2)) + ylim(c(0,3.5)) +
+  xlab(list_pept[1])
+p2 = plot_distrib(sample, group1 = "1", group2 = "7", peptide = list_pept[2]) +
+  xlim(c(-2,2)) + ylim(c(0,3.5)) + ylab("") +
+  xlab(list_pept[2])
+p3 = plot_distrib(sample, group1 = "1", group2 = "7", peptide = list_pept[3]) +
+  xlim(c(-2,2)) + ylim(c(0,3.5)) + ylab("") +
+  xlab(list_pept[3])
+p4 = plot_distrib(sample, group1 = "1", group2 = "7", peptide = list_pept[4]) +
+  xlim(c(-2,2)) + ylim(c(0,3.5)) +
+  xlab(list_pept[4])
+p5 = plot_distrib(sample, group1 = "1", group2 = "7", peptide = list_pept[5]) +
+  xlim(c(-2,2)) + ylim(c(0,3.5)) + ylab("") +
+  xlab(list_pept[5])
+p6 = plot_distrib(sample, group1 = "1", group2 = "7", peptide = list_pept[6]) +
+  xlim(c(-2,2)) + ylim(c(0,3.5)) + ylab("") +
+  xlab(list_pept[6])
+p7 = plot_distrib(sample, group1 = "1", group2 = "7", peptide = list_pept[7]) +
+  xlim(c(-2,2)) + ylim(c(0,3.5)) +
+  xlab(list_pept[7])
+p8 = plot_distrib(sample, group1 = "1", group2 = "7", peptide = list_pept[8]) +
+  xlim(c(-2,2)) + ylim(c(0,3.5)) + ylab("") +
+  xlab(list_pept[8])
+p9 = plot_distrib(sample, group1 = "1", group2 = "7", peptide = list_pept[9]) +
+  xlim(c(-2,2)) + ylim(c(0,3.5)) + ylab("") +
+  xlab(list_pept[9])
+
+gg = cowplot::plot_grid(p1, p2, p3, p4, p5, p6, p7, p8, p9, nrow = 3, ncol = 3, label_x = 'Bla') 
+
+# ggsave('FIGURES/multivariate_marginals.png', gg, dpi = 600, width = 6400, height = 3600, units = "px")
